a1d267c86a3668dbc1e61798151cf9df2333ed55
Gaurav Kumar <gk956842@gmail.com>
2022-08-07 12:42:38 +0530

    index serve changes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const answerRouter = require('./routers/answerRouter/answer.router');
 const articleRouter = require('./routers/articleRouter/article.router');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -33,11 +34,10 @@ app.use('/api/v1/', articleRouter)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    app.get("*", function (req, res) {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
-
-app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.listen(PORT, () => {
     console.log("server is running on the port", PORT);
